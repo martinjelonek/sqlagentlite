6d79a8187d330daadd2707b9590da4b2aad997d7
Martin Jelonek <104428225+MJelonek@users.noreply.github.com>
2023-09-20 11:07:34 +0200

    [#8] Create execute command

---
+++ sal/Commands/ExecuteCommand.cs
@@ -0,0 +1,12 @@
+using SAL.Constants.Messages;
+
+namespace SAL.Commands
+{
+    class ExecuteCommand
+    {
+        public static void Run()
+        {
+            Console.WriteLine(Msg.EXECUTE_START_MESSAGE);
+        }
+    }
+}
